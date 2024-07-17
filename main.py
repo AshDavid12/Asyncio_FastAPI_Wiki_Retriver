1ce9c3da467c142c03fd5a0a7b3894b52b130f97
AshDavid12 <gaashdavid@gmail.com>
2024-07-17 13:29:39 +0300

    not working

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from pydantic import BaseModel
 import requests
 import uvicorn
 import httpx
+import asyncio
 
 app = FastAPI()
 
@@ -15,17 +16,16 @@ class Article(BaseModel):
 
 @app.post("/articles/") #this is to accept an article name 
 async def accept_article(article:Article): #create Article class instance called article
-    return await get_first_par(article.name) #add AWAIT
+    return get_first_par(article.name) #add AWAIT
 
 @app.get("/articles/{article_name}")
 async def get_first_par(article_name:str):
     url = f"https://en.wikipedia.org/api/rest_v1/page/summary/{article_name}" #from the wiki docs
-    response = await client.get(url) #add AWAIT
+    response = requests.get(url)
     data = response.json() #convert reponse to json object
     state["first_paragraph"] = data['extract'].split('\n')[0] 
     return {"article_name":article_name,"first_paragraph": state["first_paragraph"]}
 
-client = httpx.AsyncClient(follow_redirects=True)# keep instance of client
 @app.get("/")
 def read_root():
         return {"message": f"Most recent article: {state['first_paragraph']}"}
